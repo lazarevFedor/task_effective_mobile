0432451bb35c0c18520f7294566ff3e4de390c1d
lazarevFedor <lazarevfedor3@gmail.com>
2026-01-28 23:02:25 +0300

    add: repository template

--- internal/repositories/subscriptions.go
+++ internal/repositories/subscriptions.go
@@ -1,1 +1,33 @@
 package repositories
+
+import (
+	"context"
+	"fmt"
+	"task_effective_mobile/pkg/postgres"
+
+	_ "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SubscriptionsRepository struct {
+	pg *pgxpool.Pool
+}
+
+func NewSubscriptionsRepository(ctx context.Context, cfg postgres.Config) (*SubscriptionsRepository, error) {
+	pool, err := postgres.New(ctx, cfg, "users")
+	if err != nil {
+		return nil, fmt.Errorf("NewSubscriptionsRepository: failed to connect to postgres: %w", err)
+	}
+	return &SubscriptionsRepository{pg: pool}, nil
+}
+
+func (r *SubscriptionsRepository) CreateSub() {}
+
+func (r *SubscriptionsRepository) GetSub() {}
+
+func (r *SubscriptionsRepository) UpdateSub() {}
+
+func (r *SubscriptionsRepository) DeleteSub() {}
+
+func (r *SubscriptionsRepository) GetSubsList() {}
