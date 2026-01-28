775c108eaa68e42375fde8b17aa4702858ccb1d2
lazarevFedor <lazarevfedor3@gmail.com>
2026-01-28 10:42:03 +0300

    add: main function and http server template

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"task_effective_mobile/internal/server"
+	"task_effective_mobile/pkg/logger"
+)
+
+func main() {
+	ctx := logger.WithLogger(context.Background(), slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+	_ = server.Start(ctx)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return fmt.Errorf("start: error while starting http server: %w", err)
+	}
+	return nil
+}
