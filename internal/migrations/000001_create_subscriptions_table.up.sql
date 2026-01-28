787db8b9eef56cbefe0285e8b3afb73894fe760b
lazarevFedor <lazarevfedor3@gmail.com>
2026-01-28 20:56:58 +0300

    add: migrations

---
+++ internal/migrations/000001_create_subscriptions_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id SERIAL PRIMARY KEY,
+    service_name VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL CHECK (price >= 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
