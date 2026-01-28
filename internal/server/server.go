50f537cf0c14ee6099d21c2c89deb8b85eb2bd3f
lazarevFedor <lazarevfedor3@gmail.com>
2026-01-28 11:01:29 +0300

    update: ports has been taken from env file

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"task_effective_mobile/internal/config"
 )
 
 func Start(ctx context.Context) error {
 	mux := http.NewServeMux()
-	err := http.ListenAndServe(":8080", mux)
+	cfg, err := config.New()
+	if err != nil {
+		return fmt.Errorf("start: failed to load config: %w", err)
+	}
+	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), mux)
 	if err != nil {
 		return fmt.Errorf("start: error while starting http server: %w", err)
 	}
